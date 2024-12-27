1650242ca6c3cc6458be72771b514f35100dbf3e
omarHJ <106180546+omarHJ@users.noreply.github.com>
2024-12-27 19:03:37 +0200

    Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,20 @@
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-            <a className="navbar-brand" href="#">Company name</a>
+            <Link className="navbar-brand" to="">Company name</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ms-auto">
-                    <li className="nav-item"><a className="nav-link" href="Features">Features</a></li>
-                    <li className="nav-item"><a className="nav-link" href="Enterprise">Enterprise</a></li>
-                    <li className="nav-item"><a className="nav-link" href="Support">Support</a></li>
-                    <li className="nav-item"><a className="nav-link" href="Pricing">Pricing</a></li>
+                    <li className="nav-item"><Link className="nav-link" to="Features">Features</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="Enterprise">Enterprise</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="Support">Support</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to="Pricing">Pricing</Link></li>
                 </ul>
             </div>
         </div>
